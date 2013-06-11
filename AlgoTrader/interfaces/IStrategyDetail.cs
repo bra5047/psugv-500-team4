24cff22441aefe9047fe30547e7f72c1174be169
bra5047 <bra5047@psu.edu>
2013-06-10 22:11:27 -0400

    added SMA metric class for strategy

--- AlgoTrader/interfaces/IStrategyDetail.cs
+++ AlgoTrader/interfaces/IStrategyDetail.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace AlgoTrader.Interfaces
 {
-    interface IStrategyDetail
+    public interface IStrategyDetail
     {
         ISymbol symbol { get; set; }
     }
--- AlgoTrader/interfaces/IStrategySummary.cs
+++ AlgoTrader/interfaces/IStrategySummary.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace AlgoTrader.Interfaces
 {
-    interface IStrategySummary
+    public interface IStrategySummary
     {
         ISymbol symbol { get; set; }
     }
