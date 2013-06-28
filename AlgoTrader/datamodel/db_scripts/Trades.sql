5fa45cc1d9500c6f948d732c46c3afdb69335e1f
bra5047 <bra5047@psu.edu>
2013-06-28 18:30:13 -0400

    updated database table scripts

--- AlgoTrader/datamodel/db_scripts/Trades.sql
+++ AlgoTrader/datamodel/db_scripts/Trades.sql
@@ -1,4 +1,3 @@
-/****** Object:  Table [dbo].[Trades]    Script Date: 6/10/2013 1:16:37 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -7,17 +6,21 @@ GO
 
 CREATE TABLE [dbo].[Trades](
 	[TradeId] [int] IDENTITY(1,1) NOT NULL,
+	[InitialQuantity] [int] NULL,
 	[quantity] [int] NOT NULL,
 	[price] [float] NOT NULL,
 	[timestamp] [datetime] NOT NULL,
 	[type] [int] NOT NULL,
+	[TransactionId] [nvarchar](max) NULL,
+	[Status] [int] NOT NULL,
 	[SymbolName] [nvarchar](128) NULL,
 	[PositionId] [int] NULL,
+	[RelatedTradeId] [int] NULL,
  CONSTRAINT [PK_dbo.Trades] PRIMARY KEY CLUSTERED 
 (
 	[TradeId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
 
@@ -33,6 +36,13 @@ REFERENCES [dbo].[Symbols] ([name])
 GO
 
 ALTER TABLE [dbo].[Trades] CHECK CONSTRAINT [FK_dbo.Trades_dbo.Symbols_SymbolName]
+GO
+
+ALTER TABLE [dbo].[Trades]  WITH CHECK ADD  CONSTRAINT [FK_dbo.Trades_dbo.Trades_RelatedTradeId] FOREIGN KEY([RelatedTradeId])
+REFERENCES [dbo].[Trades] ([TradeId])
+GO
+
+ALTER TABLE [dbo].[Trades] CHECK CONSTRAINT [FK_dbo.Trades_dbo.Trades_RelatedTradeId]
 GO
 
 
